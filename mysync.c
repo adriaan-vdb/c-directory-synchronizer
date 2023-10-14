85d2b385e970954e4c30485a9a51c4864f483254
Checkmate1941 <adriaan.vanderberg@outlook.com>
2023-10-14 18:11:28 +0800

    -n and-v

--- mysync.c
+++ mysync.c
@@ -332,7 +332,10 @@ void analyse_files()
                 // Analyse each entry - two cases
                 // 1. Only 1 entry (just copy over to every other directory)
                 if (analysis_index == 1)
-                {
+            {
+                    if (flags.v){
+                        printf("FILE to be created: %s\n", (char *)relative_location);
+                    }
                     for (int j = 0; j < ndirectories; j++)
                     {
                         if (sync_index(analysis[0].file) == j)
@@ -349,6 +352,9 @@ void analyse_files()
                 // 2. More than 1 entry i.e. find most up to date version and copy to every other directory
                 else if (analysis_index != 0)
                 {
+                    if (flags.v){
+                            printf("Latest Version: %s\n", (char *)relative_location);
+                        }
                     // Find the most up to date version (latest modification time)
                     int latest_index = 0; // Stores the index of the most up to date file version
                     for (int j = 1; j < analysis_index; j++)
